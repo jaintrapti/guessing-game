dbdd1404e2576f6c9ad677659d0a164017ccd217
atishay <atishayjain759@gmail.com>
2020-10-01 18:38:12 +0530

    fixed the while condition for no of chances , it was giving 20 chances before now only 4 as written in rule

--- game50no.py
+++ game50no.py
@@ -1,19 +1,16 @@
 import random
-x = random.randint(1,50)
-y=0
+x = random.randint(1, 50)
+y = 0
 print("play guessing game,only four chance there")
-while(y<=25):
-    z=int(input("guess between 1 to 50\n"))
-    if z<x:
+while(y < 4):
+    z = int(input("guess between 1 to 50\n"))
+    if z < x:
         print("lower than guess value")
-    elif z>x:
+    elif z > x:
         print("greater than guess value")
-    elif z==x:
-        print("congrates you guess right!1")
-        break;
-    y=y+1
-    print(y)
-if(y==20):
-   print("you loose all chance upppss!!")
-                                                      
-        
+    elif z == x:
+        print("congrates you guess right!!!")
+        break
+    y = y+1
+if(y == 4):
+    print("you loose all chance upppss!")
